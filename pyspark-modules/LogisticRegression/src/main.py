08d9fe69f60244aacce21cb9dfad4c7c029e3792
Kuner <yunky.dong@gmail.com>
2017-07-27 11:10:50 +0800

    add pyspark/LogisticRegression source

---
+++ pyspark-modules/LogisticRegression/src/main.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pyspark.ml.classification import LogisticRegression
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.read.format("libsvm").load("../../../data/sample_libsvm_data.txt")
+lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
+model = lr.fit(df)
+
+trainingSummary = model.summary
+trainingSummary.roc.show()
--- pyspark-modules/LogisticRegression/test.sh
+++ pyspark-modules/LogisticRegression/test.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env sh
 
-work_dir=$(pwd)
+src/main.py
